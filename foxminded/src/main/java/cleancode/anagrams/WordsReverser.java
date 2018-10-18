5ba03d5b072a28c9137ed782f50a74c1ccf9f7f6
vsokolov <vadim.sokolov@gmail.com>
2018-10-18 09:46:18 +0300

    changed SortedMap to Map and an array name.

--- foxminded/src/main/java/cleancode/anagrams/WordsReverser.java
+++ foxminded/src/main/java/cleancode/anagrams/WordsReverser.java
@@ -1,15 +1,15 @@
 package cleancode.anagrams;
 
 import java.util.Collections;
-import java.util.SortedMap;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class WordsReverser {
 	
 	public String reverseWords(String wordsToReverse) {
 		StringBuilder result = new StringBuilder();
-		String[] splitWordsBySpaces = splitStringIntoWords(wordsToReverse);
-		for (String word : splitWordsBySpaces) {
+		String[] words = splitStringIntoWords(wordsToReverse);
+		for (String word : words) {
 			result.append(createReversedWord(word) + " ");
 		}
 		return result.toString().trim();
@@ -21,7 +21,7 @@ public class WordsReverser {
 	
 	private String createReversedWord(String word) {
 		StringBuilder result = getReversedWordWithoutNonLetters(word);
-		SortedMap<Integer, Character> nonLettersPositions = getNonLettersPositions(word);
+		Map<Integer, Character> nonLettersPositions = getNonLettersPositions(word);
 		setNonLettersToReversedWord(result, nonLettersPositions);
 		return result.toString();
 	}
@@ -36,8 +36,8 @@ public class WordsReverser {
 		return wordWithoutNonLetters.reverse();
 	}
 	
-	private SortedMap<Integer, Character> getNonLettersPositions(String word) {
-		SortedMap<Integer, Character> nonLetters = new TreeMap<>();
+	private Map<Integer, Character> getNonLettersPositions(String word) {
+		Map<Integer, Character> nonLetters = new TreeMap<>();
 		for (int i = 0; i < word.length(); i++) {
 			if (!Character.isLetter(word.charAt(i))) {
 				nonLetters.put(i, word.charAt(i));
@@ -46,7 +46,7 @@ public class WordsReverser {
 		return nonLetters;
 	}
 	
-	private void setNonLettersToReversedWord(StringBuilder result, SortedMap<Integer, Character> nonLetters) {
+	private void setNonLettersToReversedWord(StringBuilder result, Map<Integer, Character> nonLetters) {
 		for (int i = 0; i < (result.length() + nonLetters.size()); i++) {
 			if (nonLetters.containsKey(i)) {
 				result.insert(i, nonLetters.get(i));
